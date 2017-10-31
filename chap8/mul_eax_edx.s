f27cd3d68175b394ec644c3e22dc48f0e263d81b
the96 <200372c@ugs.kochi-tech.ac.jp>
2017-10-31 14:05:20 +0900

    1.8.2 complete

---
+++ chap8/mul_eax_edx.s
@@ -0,0 +1,31 @@
+    section .text
+    global mul_eax_edx
+mul_eax_edx:
+    push ebx
+    push ecx
+    push edx
+    push esi
+    push edi
+
+    mov cl, 0      ;counter reset
+    mov esi, 0
+loop:
+    mov ebx, edx
+    and ebx, 1      ;get last bit
+    cmp ebx, 1      ;last bit is 1
+    jne skip
+    mov edi, eax
+    shl edi, cl     ;edi *= 2 ^ cl
+    add esi, edi    ;sum
+skip:
+    shr edx, 1
+    inc ecx
+    cmp edx, 0
+    jg loop
+
+    pop edi
+    pop esi
+    pop edx
+    pop ecx
+    pop ebx
+    ret
