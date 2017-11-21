6f921665efb02af5d78507585bee33b01bcc222d
the96 <200372c@ugs.kochi-tech.ac.jp>
2017-11-21 17:09:37 +0900

    平方根まで試し割りする素数計算

--- chap11/isPrime.s
+++ chap11/isPrime.s
@@ -1,49 +1,27 @@
     section .text
     global isPrime
 isPrime:
-    push ebp
-    mov ebp, esp
-    push ebx
-    mov eax, [ebp + 8]
-    mov ebx, [ebp + 12]
-    mov [ebx], byte 1         ; 0は素数ではない(配列の初期化のことを考えて真偽値は反転)
-    mov [ebx + 1], byte 1     ; 1は素数ではない
-    cmp eax, [maxPrime]         ; 既に計算済みの範囲なら計算しない
-    jle endPrime
-    mov ecx, 2 
-
-eratosthenes:
-    mov edx, ecx
-    cmp byte [ebx + edx], 1   ; 素数でないなら飛ばす
-    je skipLoop0
+    mov eax, [esp + 4]
+    mov ecx, 2
+    cmp eax, ecx
+    jl notPrime
 loop0:
-    add edx, ecx                ; 最大値(2^32)を越えたら終了
-    jc skipLoop0                ; つまり、桁溢れが発生したら終了
-    cmp edx, eax
-    jg skipLoop0
-    mov [ebx + edx], byte 1   ; 素数でないのでチェックをつける
-    jmp loop0
-skipLoop0:
-    cmp ecx, maxNum
-    je endPrime
+    cmp eax, ecx
+    je itsPrime
+    mov edx, 0
+    div ecx
+    cmp edx, 0
+    je notPrime
+    cmp eax, ecx
+    jl itsPrime
     inc ecx
-    cmp ecx, eax
-    jge endPrime
-    jmp eratosthenes
+    mov eax, [esp + 4]
+    jmp loop0
 
-endPrime:
-    cmp eax, [maxPrime]
-    jle notMax
-    mov [maxPrime], eax
-notMax:
-    mov ecx, 0
-    mov cl, [ebx + eax]
-    mov eax, ecx
-    pop ebx
-    pop ebp
+itsPrime:
+    mov eax, 1
     ret
 
-maxNum: equ 4294967295
-    section .data
-maxPrime:   dd  2           ; 既に検索したもっとも大きな素数を代入しておく
-                            ; 複数回実行したとき、計算量を減らすため
+notPrime:
+    mov eax, 0
+    ret
--- chap11/test_prime.c
+++ chap11/test_prime.c
@@ -1,17 +1,11 @@
 #include <stdio.h>
 
-extern int isPrime(unsigned int a, char b[]);
+extern int isPrime(unsigned int a);
 
 int main() {
     unsigned int x;
-    unsigned int max = 0xfffffff;//1048576;
-    char sieve[max];
     while (scanf("%u", &x) != EOF) {
-        if (x > max) {
-            printf("over number of max\n");
-            continue;
-        }
-        if (!isPrime(x,sieve)) {
+        if (isPrime(x)) {
             printf("prime\n");
         } else {
             printf("not prime\n");
