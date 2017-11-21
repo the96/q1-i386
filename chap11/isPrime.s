49e118605dc7d448b67d8346252d2c1f3872279f
the96 <200372c@ugs.kochi-tech.ac.jp>
2017-11-21 17:17:59 +0900

    コメントを追加

--- chap11/isPrime.s
+++ chap11/isPrime.s
@@ -1,21 +1,21 @@
     section .text
     global isPrime
 isPrime:
-    mov eax, [esp + 4]
-    mov ecx, 2
-    cmp eax, ecx
+    mov eax, [esp + 4]      ; 引数を取得
+    mov ecx, 2              ; 除数の初期値をセット
+    cmp eax, ecx            ; 2未満の数は素数ではない
     jl notPrime
 loop0:
-    cmp eax, ecx
+    cmp eax, ecx            ; 除数と被除数が一致した場合は素数
     je itsPrime
     mov edx, 0
-    div ecx
-    cmp edx, 0
+    div ecx                 ; 試し割り
+    cmp edx, 0              ; 余りが0なら素数ではない
     je notPrime
-    cmp eax, ecx
+    cmp eax, ecx            ; 商が除数より小さい場合、除数は平方根を越えている
     jl itsPrime
-    inc ecx
-    mov eax, [esp + 4]
+    inc ecx                 ; 次の除数へ
+    mov eax, [esp + 4]      ; 被除数をセット
     jmp loop0
 
 itsPrime:
